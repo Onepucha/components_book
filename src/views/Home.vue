27890cf8fb9857e342f63257dd3f9d731765bf11
a.petushkov <62595753+Onepucha@users.noreply.github.com>
2021-04-16 15:19:56 +0300

    Add redirect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,10 @@
       <a href="#" class="black-text" @click.prevent="logout">Выйти
       </a>
     </div>
+    <div class="page-title">
+      <a href="#" class="black-text" @click.prevent="redirect">Редирект
+      </a>
+    </div>
   </div>
 </template>
 
@@ -14,6 +18,9 @@
       async logout() {
         await this.$store.dispatch('logout')
         this.$router.push('/login?message=logout')
+      },
+      redirect() {
+        window.location.assign('http://localhost:6006/?test=123');
       }
     },
   }
