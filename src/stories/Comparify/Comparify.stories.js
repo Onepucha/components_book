363c1dc53592ebcc5c1f5b269920f5489bfb3090
a.petushkov <62595753+Onepucha@users.noreply.github.com>
2021-05-25 13:28:41 +0300

    Add fix styles

--- src/stories/Comparify/Comparify.stories.js
+++ src/stories/Comparify/Comparify.stories.js
@@ -22,7 +22,7 @@ const Template = (args, {
     <div class="container">
         <div class="row">
             <div class="col-lg-24 col-xs-12">
-                <lmm-comparify class="bg-white h-64 w-full" value="45">
+                <lmm-comparify class="bg-white h-64 w-full" value="50">
                     <img slot="first" class="h-64 object-fit-cover h-full w-full"
                         src="https://images.unsplash.com/photo-1525936607120-6a787fb05d6a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3cb09a29812d04718a5855dc61442612&auto=format&fit=crop&w=800&q=90"
                         alt="">
